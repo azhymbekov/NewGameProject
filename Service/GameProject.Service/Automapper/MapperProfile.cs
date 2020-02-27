f8157b1d0b3df53034af3c87a5673c92833533ff
Azhymbekov <Azhymbekovv@gmail.com>
2020-02-27 18:21:55 +0600

    Push After Refactoring

---
+++ Service/GameProject.Service/Automapper/MapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using GameProject.Data.Models.Game;
+using GameProject.Data.Models.Users;
+using GameProject.Service.Common.UserService.Models;
+using GameProject.Service.Common.WordService.Models;
+
+namespace GameProject.Service.Automapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Word, WordModel>()
+                .ForMember(x => x.TryCount,
+                    opt => opt.MapFrom(x => x.SecretWord.Length - 2));
+            CreateMap<WordModel, Word>();
+
+
+            CreateMap<User, UserModel>();
+            CreateMap<UserModel, User>();
+        }
+    }
+}
